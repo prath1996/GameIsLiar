4732e9f5d4cad0be3990b40249c31712bf6192a5
Prathamesh Mahajan <prathamesh.gameberrylabs.com>
2019-08-26 01:05:40 +0530

    chapter 2 UI added

---
+++ Assets/Chapter2Layer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter2Layer : MonoBehaviour
+{
+    private int sceneTop = 800;
+    private int sceneBottom = 0;
+    private int sceneWidth = 300;
+    private int brickWidth = 30;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float width;
+    private float moveSpeed = 1f;
+    private Vector2 velocity;
+    void MoveLeft()
+    {
+        Vector2 dir = new Vector2(-1, 0);
+        velocity = dir * moveSpeed;
+    }
+
+    void MoveRight()
+    {
+        Vector2 dir = new Vector2(1, 0);
+        velocity = dir * moveSpeed;
+        Debug.LogError("Right");
+    }
+
+    void MovePlayer(Vector2 pos)
+    {
+        if (pos.x > width)
+        {
+            MoveLeft();
+        } 
+        else
+        {
+            MoveRight();
+        }
+        Vector2 currPos = this.gameObject.transform.position;
+        this.gameObject.transform.position = currPos + velocity * Time.deltaTime;
+    }
+
+    void Start()
+    {
+        width = (float)Screen.width / 2.0f;
+    }
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            MovePlayer(touch.position);
+        }
+        else
+        {
+            velocity = Vector2.zero;
+        }
+
+        if (Input.touchCount == 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MovePlayer(Input.mousePosition);
+            }
+            else
+            {
+                velocity = Vector2.zero;
+            }
+        }
+    }
+}
